b01e6fbfb7820c954f3fa489f8614645870a3c6e
etorth <anhonghe@gmail.com>
2019-01-18 00:46:38 +0000

    init

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename: main.cpp
+ *        Created: 01/18/2019 00:39:15
+ *    Description: 
+ *
+ *        Version: 1.0
+ *       Revision: none
+ *       Compiler: gcc
+ *
+ *         Author: ANHONG
+ *          Email: anhonghe@gmail.com
+ *   Organization: USTC
+ *
+ * =====================================================================================
+ */
+
+int main()
+{
+    return 0;
+}
